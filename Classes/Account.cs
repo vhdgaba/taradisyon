23727b5c3985c99097d9b64fe90900f9b7fd1e14
kylealelis <aleliskyleangelo@yahoo.com>
2020-02-22 17:17:17 +0800

    init Classes

---
+++ Classes/Account.cs
@@ -0,0 +1,14 @@
+namespace Account
+{
+    class UserDetails
+    {
+        public string FirstName;
+        public string LastName;
+        public string MiddleInitial;
+        public char Gender;
+        public string Email;
+        public string Password;
+        public DateTime BirthDate;
+        public string Nationality;
+    }
+}
